6a9e1bcb202d240f062bb403c831792aa05e0778
Miika Moilanen <miika.moilanen@tietoevry.com>
2021-02-17 13:32:27 +0200

    Added evil plus bunch of other stuff

--- init.el
+++ init.el
@@ -7,9 +7,25 @@
 (menu-bar-mode -1)
 ; (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Fira Code" :height 120)
+(set-face-attribute 'default nil :font "Fira Code" :height 125 :weight 'ultra-light)
 
-(load-theme 'wombat t)
+;; (load-theme 'wombat t)
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-bubblegum t)
+  (kaolin-treemacs-theme))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+;; NOTE: The first time you load your configuration on a new machine, you'll
+;; need to run the following command interactively so that mode line icons
+;; display correctly:
+;;
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -60,7 +76,116 @@
   :config
   :init (ivy-mode 1))
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+
+(show-paren-mode 1)
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish wich-key-mode
+  :config (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(use-package god-mode)
+
+(use-package general
+  :config
+  (general-create-definer miika/leader-keys
+    :keymaps '(normal visual emacs)
+    :prefix "SPC")
+
+  (miika/leader-keys
+    "SPC" '(counsel-M-x :which-key "M-x")
+
+    "/" '(swiper :which-key "swiper")
+
+    "e" '(:ignore t :which-key "eval")
+    "ed" '(eval-defun :which-key "eval defun")
+
+    ;; Toggle
+    "t" '(:ignore t :which-key "toggle")
+    "tt" '(counsel-load-theme :which-key "load theme")
+    "ts" '(hydra-text-scale/body :which-key "scale text")
+
+    ;; Window management
+    "w"  '(:ignore t :which-key "window")
+    "wv" '(evil-window-vsplit :which-key "split window vertically")
+    "ww" '(evil-window-next :which-key "next window")
+
+    ;; Files
+    "f" '(:ignore t :which-key "file")
+    "ff" '(find-file :which-key "find file")
+
+    ;; Help
+    "h" '(:ignore t :which-key "help")
+    "hv" '(describe-variable :which-key "describe a variable")
+    "hf" '(describe-function :which-key "describe a function")
+    ))
+
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package evil-commentary
+  :after evil
+  :init (evil-commentary-mode))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(god-mode kaolin-themes doom-modeline ivy use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
