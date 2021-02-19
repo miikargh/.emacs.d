8dc39aa80778e87fa3c0526537183f5cc86c3b5f
Miika Moilanen <miika.moilanen@tietoevry.com>
2021-02-19 10:25:01 +0200

    Hoisted use-package init

--- init.el
+++ init.el
@@ -9,6 +9,27 @@
 
 (set-face-attribute 'default nil :font "Fira Code Light" :height 125 :weight 'ultra-light)
 
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+
+
+
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
 ;; (load-theme 'wombat t)
 (use-package kaolin-themes
   :config
